cb4fb80817835e6420fbc85cb5f0afe9997ce2f0
DieuDonné ASSOTI <ddassoti33@gmail.com>
2026-02-06 10:14:22 +0000

    Remove stylesheet link from formulaire1.php

--- exo_php/6fevrier/formulaire1.php
+++ exo_php/6fevrier/formulaire1.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>formulaire 6février</title>
-    <link rel="stylesheet" href="formulaire.css">
+   
 </head>
 <body>
     <center>
