b2661d6d0e7e006afd18545a9b42dc072fd01559
DieuDonné <ddassoti33@gmail.com>
2026-02-06 22:28:57 +0000

    6fevrier

--- exo_python/exo_6fevrier.py
+++ exo_python/exo_6fevrier.py
@@ -1,20 +1,19 @@
-# 1. Créer une liste vide
+
 eleves = []
 
-# 2. Répéter l'opération pour 3 élèves
 for i in range(3):
     print(f"\nÉlève {i + 1}")
     nom = input("Nom de l'élève : ")
     moyenne = float(input("Moyenne de l'élève : "))
 
-    # 3. Ajouter les infos dans la liste
+    
     eleve = {
         "nom": nom,
         "moyenne": moyenne
     }
     eleves.append(eleve)
 
-# 5. Affichages
+
 print("\n--- TOUS LES ÉLÈVES ---")
 for e in eleves:
     print(f"{e['nom']} - Moyenne : {e['moyenne']}")
